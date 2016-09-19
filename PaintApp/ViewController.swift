de2dbf45baec631cf2b8189ee2cddbbee6ecff7e
RAVI RANDERIA <raviranderia@ravis-macbook-pro.local>
2016-09-19 14:36:38 -0400

    Initial Commit

---
+++ PaintApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PaintApp
+//
+//  Created by RAVI RANDERIA on 9/19/16.
+//  Copyright © 2016 RAVI RANDERIA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
